c24a45df5e2a89e929c8408bb3653a40b8b75b01
IlhamTajuddin <40162249+IlhamTajuddin@users.noreply.github.com>
2018-07-04 22:22:17 +0700

    hanif

---
+++ application/views/navbar.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+			<![endif]-->
+		</head>
+		<body>
+			
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav">
+							<li><a href="<?php echo base_url() ?>index.php/anak/">Data Warga</a></li>
+							<li><a href="<?php echo base_url() ?>index.php/pegawai/">Pengajuan Cetak Surat</a></li>
+							<li><a href="<?php echo base_url() ?>index.php/jabatan/">Jadwal Ronda</a></li>
+							<li><a href="<?php echo base_url() ?>index.php/login/logout">Logout</a></li>
+						</ul>
+						
+						
+						
+							
+							</div><!-- /.navbar-collapse -->
+						</div><!-- /.container-fluid -->
+					</nav>
+
+					<!-- jQuery -->
+					<script src="//code.jquery.com/jquery.js"></script>
+					<!-- Bootstrap JavaScript -->
+					<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+					<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+					<script src="Hello World"></script>
+				</body>
+				</html>
